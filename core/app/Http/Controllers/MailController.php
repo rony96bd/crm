348a68703a0d58f986547dbb316a1fec3ce12242
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-07-21 20:05:45 +0600

    fix email

--- core/app/Http/Controllers/MailController.php
+++ core/app/Http/Controllers/MailController.php
@@ -8,9 +8,10 @@ use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    public static function sendSignupEmail($name, $email, $verification_code){
+    public static function sendSignupEmail($firstname, $email, $verification_code)
+    {
         $data = [
-            'name' => $name,
+            'name' => $firstname,
             // 'verification_code' => $verification_code
         ];
         Mail::to($email)->send(new SignupEmail($data));
