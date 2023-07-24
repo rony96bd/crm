121f908e5734def633b047c49e45e3454b4e5bfc
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-07-24 22:33:22 +0600

    fix3

--- core/resources/views/mail/signup-email.blade.php
+++ core/resources/views/mail/signup-email.blade.php
@@ -8,7 +8,7 @@ Your Registration is Complete.
 Please Click the link below to next process.
 <br/>
 <br/>
-<a style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#1d1d6e;border-radius:30px;width:auto;border-top:0px solid #5841d8;font-weight:700;border-right:0px solid #5841d8;border-bottom:0px solid #5841d8;border-left:0px solid #5841d8;padding-top:5px;padding-bottom:5px;font-family:Helvetica,Arial,Sans-serif;font-size:16px;text-align:center;word-break:keep-all;width: 85px;" href="https://crm.appliuk.com/">    Login    </button></a>
+<a style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#1d1d6e;border-radius:30px;width:auto;border-top:0px solid #5841d8;font-weight:700;border-right:0px solid #5841d8;border-bottom:0px solid #5841d8;border-left:0px solid #5841d8;padding-top:5px;padding-bottom:5px;font-family:Helvetica,Arial,Sans-serif;font-size:16px;text-align:center;word-break:keep-all;width: 85px;" href="https://crm.appliuk.com/student/dashboard"> Login </button></a>
 <br/>
 <br/>
 Thanks
