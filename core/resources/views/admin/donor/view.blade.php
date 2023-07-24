a49e66a24404d84299efc3a081e8d7afbff94208
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-07-24 09:20:28 +0600

    add hr

--- core/resources/views/admin/donor/view.blade.php
+++ core/resources/views/admin/donor/view.blade.php
@@ -81,8 +81,8 @@
                                             foreach ($engtests as $engtest) {
                                                 $engtestview .= $engtest. ', ';
                                             }
-                                            $engtestview = rtrim($engtestview,', '); 
-                                            echo $engtestview;                                   
+                                            $engtestview = rtrim($engtestview,', ');
+                                            echo $engtestview;
                                         @endphp
                                     </p>
                                 </div>
@@ -132,6 +132,7 @@
                                     <p class="text-muted mb-0">{{ __($donor->course) }}</p>
                                 </div>
                             </div>
+                            <hr>
                             <div class="row">
                                 <div class="col-sm-3">
                                     <p class="mb-0 font-weight-bold">Submitted Documents</p>
